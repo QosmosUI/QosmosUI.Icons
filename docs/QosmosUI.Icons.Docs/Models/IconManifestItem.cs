23557f3ca184ccb9e78813097a19e19f1bc19a5b
jimmy-mll <jimmy.mll@hotmail.com>
2025-04-24 23:32:13 +0200

    feat: add IconManifestItem model for icon manifest representation

---
+++ docs/QosmosUI.Icons.Docs/Models/IconManifestItem.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) QosmosUI 2025.
+// QosmosUI licenses this file to you under the MIT license.
+// See the license here https://github.com/QosmosUI/QosmosUI/blob/main/LICENSE.
+
+using System.Text.Json.Serialization;
+
+namespace QosmosUI.Icons.Docs.Models;
+
+/// <summary>
+/// Represents an item in the icon manifest.
+/// </summary>
+public sealed class IconManifestItem
+{
+    /// <summary>
+    /// Gets the unique identifier for the icon set.
+    /// </summary>
+    public required string Id { get; init; }
+
+    /// <summary>
+    /// Gets the name of the icon set.
+    /// </summary>
+    public required string Name { get; init; }
+
+    /// <summary>
+    /// Gets the name of the project associated with the icon set.
+    /// </summary>
+    public required string ProjectName { get; init; }
+
+    /// <summary>
+    /// Gets the URL of the project associated with the icon set.
+    /// </summary>
+    public required string ProjectUrl { get; init; }
+
+    /// <summary>
+    /// Gets the URL of the license under which the icon set is distributed.
+    /// </summary>
+    public required string LicenseUrl { get; init; }
+
+    /// <summary>
+    /// Gets the name or type of the license under which the icon set is distributed.
+    /// </summary>
+    public required string License { get; init; }
+
+    /// <summary>
+    /// Gets the total number of icons in the icon set.
+    /// </summary>
+    public required int IconsCount { get; init; }
+
+    /// <summary>
+    /// Gets all sets of icons.
+    /// </summary>
+    [JsonIgnore]
+    public required Dictionary<string, List<Type>> Sets { get; init; } = [];
+}
