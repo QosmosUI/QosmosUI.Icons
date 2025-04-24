b3b9990ba6dd1af1d457437c2deafd6fcbb52b65
jimmy-mll <jimmy.mll@hotmail.com>
2025-04-25 00:07:05 +0200

    feat: add IconService and configure custom RunAsQosmosApp method

--- docs/QosmosUI.Icons.Docs/Program.cs
+++ docs/QosmosUI.Icons.Docs/Program.cs
@@ -1,11 +1,16 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using QosmosUI.Icons.Docs;
+using QosmosUI.Icons.Docs.Extensions;
+using QosmosUI.Icons.Docs.Services.Icons;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services
+    .AddSingleton<IconService>()
+    .AddSingleton<IIconService>(static provider => provider.GetRequiredService<IconService>())
+    .AddSingleton(new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-await builder.Build().RunAsync();
+await builder.Build().RunAsQosmosApp();
