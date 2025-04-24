fa8133928c43a6dc8b2c761397de8bcf38e14e7a
jimmy-mll <jimmy.mll@hotmail.com>
2025-04-24 23:11:55 +0200

    feat: add Sidebar components for documentation UI

---
+++ docs/QosmosUI.Icons.Docs/Components/Sidebar/SidebarNavLink.razor.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Components;
+using QosmosUI.Icons.Docs.Extensions;
+
+namespace QosmosUI.Icons.Docs.Components.Sidebar;
+
+public partial class SidebarNavLink : ComponentBase
+{
+    [Inject]
+    [NotNull]
+    private NavigationManager? Navigation { get; set; }
+
+    [Parameter]
+    [NotNull]
+    public string? To { get; set; }
+
+    [Parameter]
+    public bool IsNew { get; set; }
+
+    [Parameter]
+    [NotNull]
+    public RenderFragment? ChildContent { get; set; }
+
+    private string AdditionalClasses =>
+        Navigation.IsMatch(To) ? "bg-accent text-accent-foreground font-medium" : "text-muted-foreground font-normal";
+}
+
