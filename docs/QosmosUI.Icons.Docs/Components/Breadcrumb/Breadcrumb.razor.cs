5beb2f5c16153b1dbc56c2704ff48ac423fa8e5e
jimmy-mll <jimmy.mll@hotmail.com>
2025-04-25 15:24:22 +0200

    feat: add Breadcrumb components for documentation layout

---
+++ docs/QosmosUI.Icons.Docs/Components/Breadcrumb/Breadcrumb.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace QosmosUI.Icons.Docs.Components.Breadcrumb;
+
+public partial class Breadcrumb : ComponentBase
+{
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+}
+
---
+++ docs/QosmosUI.Icons.Docs/Components/Breadcrumb/BreadcrumbItem.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace QosmosUI.Icons.Docs.Components.Breadcrumb;
+
+public partial class BreadcrumbItem : ComponentBase
+{
+    [Parameter]
+    public string? Title { get; set; }
+
+    [Parameter]
+    public string? Href { get; set; }
+}
+
